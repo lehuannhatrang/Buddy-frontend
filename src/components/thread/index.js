58319ac402d85cd35063b6fe6161fe17d20c331f
Le Huan <lehuannhatrang98@gmail.com>
2019-06-22 10:38:41 +0700

    updates

--- src/components/thread/index.js
+++ src/components/thread/index.js
@@ -4,49 +4,61 @@ class ThreadCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      score: 1,
+      score: 0,
       title: `I am trying to store output from calculator and display on label or
       text box in visual basic asp.net`,
-      cardlist: ["python", "c++"]
+      cardlist: ["python", "c++"],
+      star: false,
+      tags: [],
     };
     this.handleVote = this.handleVote.bind(this);
   }
+
   handleVote(amount) {
     let newscore = this.state.score + amount;
     this.setState({ score: newscore });
   }
+
+  handleClickOnTag(key){
+    let newTags = [];
+    if(this.props.cardList.indexOf(key) > -1) newTags = this.state.tags.filter(element => element !== key);
+    else newTags = this.state.tags.push(key)
+    this.setState({tags: newTags});
+  }
+  
   render() {
     return (
-      <div className="card-thread-wrapper">
-        <div className="rate-button">
+      <div className={`row card-thread-wrapper row d-flex justify-content-between pt-2 pb-2 ${this.props.className||''}`} style={{...this.props.style}}>
+        <div className="col-1 rate-button">
           <i
             className="fa fa-angle-up"
-            style={{ "font-size": 30 }}
+            style={{ "font-size": 30, cursor: "pointer" }}
             onClick={() => this.handleVote(1)}
           />
-          <div className="score"> {this.state.score}</div>
+          <div className="score pl-1" style={{cursor: "default"}}> {this.props.score || this.state.score}</div>
 
           <i
             className="fa fa-angle-down"
-            style={{ "font-size": 30 }}
+            style={{ "font-size": 30, cursor: "pointer" }}
             onClick={() => this.handleVote(-1)}
           />
         </div>
-        <div className="mid-card-wrapper">
-          <p className="title-card-thread">{this.state.title}</p>
+        <div className="col-10 mid-card-wrapper">
+          <p className="title-card-thread mb-4 mt-3">{this.props.title||this.state.title}</p>
           <div className="topic-list-wrapper">
-            {this.state.cardlist &&
-              this.state.cardlist.map((item, index) => {
+            {this.props.cardList &&
+              this.props.cardList.map((item, index) => {
                 return (
-                  <div className="topic-card" key={index}>
+                  <div className="topic-card-chosen" key={index} style={{cursor: "pointer"}} onClick={() => this.handleClickOnTag(index)}>
                     {item}
                   </div>
                 );
               })}
           </div>
         </div>
-        <div className="button-right">
-          <i className="fa fa-star-o" style={{ "font-size": 18 }} />
+        <div className="col-1 button-right">
+          {!this.state.star && <i className="fa fa-star-o" style={{ "font-size": 18, }} onClick={() => this.setState({star: true})}/>}
+          {this.state.star && <i className="fa fa-star" style={{ "font-size": 18, color: "#d1db11"}} onClick={() => this.setState({star: false})}/>}
           <i className="fa fa-comment-o" style={{ "font-size": 18 }} />
           <i className="fa fa-share" style={{ "font-size": 18 }} />
         </div>
--- src/containers/dashboard/index.js
+++ src/containers/dashboard/index.js
@@ -1,17 +1,93 @@
 import React, {Component} from 'react';
 import Header from "../../components/header/Header";
+import Thread from "../../components/thread";
 
 class DashBoard extends Component {
     constructor(props) {
         super(props);
+        this.state={
+            tags : ["Python", "NodeJS", "C++", "C#", "asp.net", "VS-Studio", "Excel", "R", "Java", "JavaScript"],
+            chosenTags: [],
+            threads: [
+                {
+                    title: "I am trying to store output from calculator and display on label or text box in visual basic asp.net",
+                    cardList: ["asp.net", "C#"],
+                    score: 5,
+                },
+                {
+                    title: "Help!! My Visual Studio doesn't work :((",
+                    cardList: ["VS-Studio"],
+                    score: 15,
+                },
+                {
+                    title: "How to recover data on my excel files",
+                    cardList: ["Excel"],
+                    score: 40,
+                },
+                {
+                    title: "How to install Jupyter NoteBook ?? Help!!",
+                    cardList: ["Python"],
+                    score: -10,
+                },
+                {
+                    title: "I need some book to learn Node JS, can you recommend somes ?",
+                    cardList: ["NodeJS"],
+                    score: 23,
+                },
+            ],
+            searchKeyWord: "",
+        }
     }
 
+    componentDidMount(){
+        // this.setState({
+        //     chosenTags: this.state.tags
+        // })
+    }
+
+    handleClickOnTag(key){
+        let newTags = this.state.chosenTags;
+        console.log(this.state.chosenTags)
+        if(this.state.chosenTags.includes(key)) newTags = this.state.chosenTags.filter(element => element !== key);
+        else newTags.push(key)
+        this.setState({chosenTags: newTags});
+      }
+
     render() {
+        const data = this.state.threads.filter(thread => thread.title.toLowerCase().includes(this.state.searchKeyWord) && ( this.state.chosenTags.length === 0 || thread.cardList.filter(tag => this.state.chosenTags.includes(tag)).length > 0))
         return(
-            <div>
-                <Header style={{position:"relative"}}/>
+            <div className="bg bg-gray">
+                <Header style={{position:"relative"}} onChange={(value) => this.setState({searchKeyWord: value.toLowerCase()})}/>
                 <div className="row">
-                    <div className="col-8 offset-2 border border-info justify-content-center align-self-center">
+                    <div className="col-8 offset-2 mt-3">
+                        <div className="row bg-white pr-3 pl-3 pt-2 pb-2" >
+                            <span className="fa fa-tag mr-4" style={{"font-size": 30, "color": "gray"}}></span>
+                            <div className="text text-muted" style={{"font-size": 23}}>Trendings</div>
+                        </div>
+                        <div className="row p-3 bg-secondary" style={{"overflow-x": "scroll", "overflow-y": "hidden"}}>
+                            {this.state.tags.map(tag => (
+                                <div className={`${this.state.chosenTags.includes(tag) ? "topic-card-chosen" : "topic-card"}`} key={tag} style={{cursor: "pointer"}} onClick={() => this.handleClickOnTag(tag)}>
+                                    {tag}
+                                </div>
+                            ))}
+                        </div>
+                        <div className="row bg-white pr-3 pl-3 pt-2 pb-2 mt-3" >
+                            <span className="fa fa-fire mr-4" style={{"font-size": 30, "color": "gray"}}></span>
+                            <div className="text text-muted" style={{"font-size": 23}}>Threads</div>
+                        </div>
+                        <div className="row pl-3 pr-3 bg-secondary" style={{"max-height" : 590, "overflow-y": "scroll"}}>
+                            {data.map(thread => (
+                                <Thread className="mt-3 mb-3" style={{width: "100%", margin : "auto"}} 
+                                title={thread.title}
+                                cardList= {thread.cardList}
+                                score= {thread.score}/>
+                            ))}
+                            {data.length === 0 && (
+                                <div className="text" style={{color: "white", "font-size": 30}}>
+                                    No Data Found
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
